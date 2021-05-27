6e43fbb167893639a187863136e6ecdb83c4d670
Shutong-Song <simonsong004@gmail.com>
2021-05-27 10:49:02 -0400

    add RBT insert

--- RBT.py
+++ RBT.py
@@ -7,8 +7,9 @@ class Node:
         self.color = None
     
 class RBT:
-    def __init__(self, root):
+    def __init__(self, root, nil):
         self.root = root
+        self.nil = nil
         
     def left_rotate(self, x):
         """
@@ -17,10 +18,10 @@ class RBT:
         """
         y = x.right
         x.right = y.left
-        if y.left:
+        if y.left != self.nil:
             y.left.parent = x
         y.parent = x.parent
-        if not x.parent:
+        if x.parent == self.nil:
             self.root = y
         elif x == x.parent.left:
             x.parent.left = y
@@ -36,10 +37,10 @@ class RBT:
         """
         x = y.left
         y.left = x.right
-        if x.right:
+        if x.right != self.nil:
             x.right.parent = y
         x.parent = y.parent
-        if not y.parent:
+        if y.parent == self.nil:
             self.root = x
         elif y.parent.left == y:
             y.parent.left = x
@@ -48,17 +49,103 @@ class RBT:
         x.right = y
         y.parent = x
         
-
+    def insert_node(self, z):
+        """
+        insert a node to the Red-black tree
+        time complexity O(lg(n)) where n is the number of nodes
+        """
+        node_p = self.nil
+        dummy = self.root
+        while dummy != self.nil:
+            node_p = dummy
+            if dummy.val > z.val:
+                dummy = dummy.left
+            else:
+                dummy = dummy.right
+                
+        z.parent = node_p
+        if node_p == self.nil:
+            self.root = z.parent
+        elif node_p.val > z.val:
+            node_p.left = z
+        else:
+            node_p.right = z
+        z.left = self.nil
+        z.right = self.nil
+        z.color = "red"
+        self.insert_fixup(z)
+    
+    def insert_fixup(self, z):
+        """
+        fix up the violation of RBT properties
+        """
+        while z.parent.color == "red": # if z.parent.color = "black" all property hold, function do nothing
+            if z.parent == z.parent.parent.left:
+                y = z.parent.parent.right
+                if y.color == "red": #if z's uncle is red, and z.parent is red, j
+                    z.parent.color = "black"
+                    y.color = "black"
+                    z.parent.parent.color = "red"
+                    z = z.parent.parent
+                elif z == z.parent.right:
+                    z = z.parent
+                    self.left_rotate(z)
+                    z.parent.color = "black"
+                    z.parent.parent.color = "red"
+                    self.right_rotate(z.parent.parent)
+            else:
+                y = z.parent.parent.left
+                if y.color == "red":
+                    z.parent.color = "black"
+                    y.color = "black"
+                    z.parent.parent.color = "red"
+                    z = z.parent.parent
+                elif z == z.parent.left:
+                    z = z.parent
+                    self.right_rotate(z)
+                    z.parent.color = "black"
+                    z.parent.parent.color = "red"
+                    self.left_rotate(z.parent.parent)
+        self.root.color = "black"
+        
+                    
+                    
+            
+        
+        
+        
 if __name__ == "__main__":
-    n1 = Node(3)
+    n1 = Node(23)
     n2 = Node(13)
-    n3 = Node(99)
-    n4 = Node(21)
-    n5 = Node(7)
+    n3 = Node(9)
+    n4 = Node(15)
+    n5 = Node(27)
+    nil = Node(float("inf"))
+    nil.color = "black"
+    n1.parent = nil 
+    n1.color = "black"
     n1.left = n2
     n2.left = n3
     n2.right = n4
+    n2.parent = n1
+    n2.color = "black"
     n1.right = n5
-    root = RBT(n1)
-    root.right_rotate(n1)
-    print(n1.parent.val)
+    n5.parent = n1
+    n3.left = nil
+    n3.right = nil
+    n3.parent = n2
+    n3.color = "red"
+    n4.left = nil
+    n4.right = nil
+    n4.parent = n2
+    n4.color = "red"
+    n5.left = nil
+    n5.right = nil
+    n5.color = "black"
+    root = RBT(n1, nil)
+    
+    #insert node
+    n6 = Node(14)
+    root.insert_node(n6)
+    print(n4.left.val)
+    
